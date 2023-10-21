8d103d9bb70787cd881ea16f3ccbf343cd59eefa
UtkrishthSrivastava <135193679+UtkrishthSrivastava@users.noreply.github.com>
2023-10-21 20:33:07 +0530

    538 changes

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,19 @@
-// this is your main.js script
+/* let isRaining = false
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
+
+
+  const now = new Date()
+  const year = now.getFullYear() // return year
+  const month = now.getMonth() + 1 // return month(0 - 11)
+  const date = now.getDate() // return date (1 - 31)
+  const hours = now.getHours() // return number (0 - 23)
+  const minutes = now.getMinutes() // return number (0 -59)
+  const seconds = now.getSeconds() // return number (0 -59)
+  
+  console.log(`${date}/${month}/${year} ${hours}:${minutes}:${seconds}`) // 4/1/2020 0:56
+   
+  */
+
+  
